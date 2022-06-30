153f65b473b3a78351ec3ff77b757defeb0d359e
yudur <duarteyudi218@gmail.com>
2022-06-30 13:47:16 -0300

    configuring the categories and the search

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
+from django.db.models import Q, Count, Case, When
 
 
 class PostIndex(ListView):
@@ -9,14 +10,55 @@ class PostIndex(ListView):
     template_name = 'posts/index.html'
     paginate_by = 6
     context_object_name = 'posts'
+    
+    def get_queryset(self):
+            qs = super().get_queryset()
+            qs = qs.order_by('-id')
+            qs = qs.annotate(
+                numero_comentarios=Count(
+                    Case(
+                        When(comentario__publicado_comentario=True, then=1)
+                    )
+                )
+            )
+            return qs
 
 
 class PostBusca(PostIndex):
-    pass
+    template_name = 'posts/post_busca.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        termo = self.request.GET.get('termo')
+
+        if not termo:
+            return qs
+
+        qs = qs.filter(
+            Q(titulo_post__icontains=termo) |
+            Q(autor_post__first_name__iexact=termo) |
+            Q(data_post__iexact=termo) |
+            Q(conteudo_post__icontains=termo) |
+            Q(excerto_post__icontains=termo)
+        )
+
+        return qs
 
 
 class PostCategoria(PostIndex):
-    pass
+    template_name = 'posts/post_categoria.html'
+
+    def get_queryset(self):
+            qs = super().get_queryset()
+
+            categoria = self.kwargs.get('categoria', None)
+
+            if not categoria:
+                return qs
+
+            qs = qs.filter(categoria_post__nome_cat__iexact=categoria)
+            
+            return qs
 
 
 class PostDetalhes(UpdateView):
